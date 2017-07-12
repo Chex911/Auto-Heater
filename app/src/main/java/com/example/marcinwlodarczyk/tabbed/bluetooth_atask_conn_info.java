5d2e611ffcff4e680d1d6859a7913b5c5f7dcc74
Yevgen Sydorenko <zvgaara@gmail.com>
2017-07-12 01:35:24 +0100

    Tabs works!

--- app/src/main/java/com/example/marcinwlodarczyk/tabbed/bluetooth_atask_conn_info.java
+++ app/src/main/java/com/example/marcinwlodarczyk/tabbed/bluetooth_atask_conn_info.java
@@ -19,14 +19,12 @@ public class bluetooth_atask_conn_info extends AsyncTask<Conn_info, Conn_info, C
     @Override
     protected Conn_info doInBackground(Conn_info... params) {
         info = new Conn_info();
-        Log.d("POST","Jobs undone");
         return info;
     }
 
     @Override
     protected void onPostExecute(Conn_info result) {
         delegate.processInfo_result(info);
-        Log.d("POST","Jobs done");
     }
 
     @Override
